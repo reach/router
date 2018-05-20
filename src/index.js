091e64fdc6f179fa25877e7de5afbb234ab1d7c6
Ryan Florence <rpflorence@gmail.com>
2018-05-19 21:18:30 -0700

    Kill the HOCs

--- src/index.js
+++ src/index.js
@@ -33,23 +33,17 @@ LocationContext.Provider.displayName = "Location.Provider";
 
 // sets up a listener if there isn't one already so apps don't need to be
 // wrapped in some top level provider
-let withLocation = Comp => {
-  let C = props => (
-    <LocationContext.Consumer>
-      {context =>
-        context ? (
-          <Comp {...context} {...props} />
-        ) : (
-          <LocationProvider>
-            {context => <Comp {...context} {...props} />}
-          </LocationProvider>
-        )
-      }
-    </LocationContext.Consumer>
-  );
-  C.displayName = `withLocation(${Comp.displayName || Comp.name})`;
-  return C;
-};
+let Location = ({ children }) => (
+  <LocationContext.Consumer>
+    {context =>
+      context ? (
+        children(context)
+      ) : (
+        <LocationProvider>{context => children(context)}</LocationProvider>
+      )
+    }
+  </LocationContext.Consumer>
+);
 
 class LocationProvider extends React.Component {
   static defaultProps = {
@@ -127,110 +121,101 @@ let BaseContext = createContext({ baseuri: "/", basepath: "/" });
 BaseContext.Consumer.displayName = "Base.Consumer";
 BaseContext.Provider.displayName = "Base.Provider";
 
-let withBase = Comp => {
-  let C = props => (
-    <BaseContext.Consumer>
-      {context => (
-        // props second so basepath can be passed in as a prop and win
-        <Comp {...context} {...props} />
-      )}
-    </BaseContext.Consumer>
-  );
-  C.displayName = `withBase(${Comp.displayName || Comp.name})`;
-  return C;
-};
-
 ////////////////////////////////////////////////////////////////////////////////
 // The main event, welcome to the show everybody.
-let Router = withBase(
-  withLocation(
-    class RouterImpl extends React.PureComponent {
-      render() {
-        let { location, navigate, basepath, children } = this.props;
-        let routes = React.Children.map(children, createRoute(basepath));
+let Router = props => (
+  <BaseContext.Consumer>
+    {baseContext => (
+      <Location>
+        {locationContext => (
+          <RouterImpl {...baseContext} {...locationContext} {...props} />
+        )}
+      </Location>
+    )}
+  </BaseContext.Consumer>
+);
+class RouterImpl extends React.PureComponent {
+  render() {
+    let { location, navigate, basepath, children } = this.props;
+    let routes = React.Children.map(children, createRoute(basepath));
 
-        let match = pick(routes, location.pathname);
+    let match = pick(routes, location.pathname);
 
-        if (match) {
-          let { params, uri, route, route: { value: element } } = match;
+    if (match) {
+      let { params, uri, route, route: { value: element } } = match;
 
-          // remove the /* from the end for child routes relative paths
-          basepath = route.default ? basepath : route.path.replace(/\*$/, "");
+      // remove the /* from the end for child routes relative paths
+      basepath = route.default ? basepath : route.path.replace(/\*$/, "");
 
-          let props = {
-            ...params,
-            uri,
-            location,
-            navigate: (to, options) => navigate(resolve(to, uri), options)
-          };
+      let props = {
+        ...params,
+        uri,
+        location,
+        navigate: (to, options) => navigate(resolve(to, uri), options)
+      };
 
-          let childRouterProps;
+      let childRouterProps;
 
-          let clone = React.cloneElement(
-            element,
-            props,
-            element.props.children || childRouterProps ? (
-              <Router {...childRouterProps}>{element.props.children}</Router>
-            ) : (
-              undefined
-            )
-          );
+      let clone = React.cloneElement(
+        element,
+        props,
+        element.props.children || childRouterProps ? (
+          <Router {...childRouterProps}>{element.props.children}</Router>
+        ) : (
+          undefined
+        )
+      );
 
-          return (
-            <BaseContext.Provider value={{ baseuri: uri, basepath }}>
-              {clone}
-            </BaseContext.Provider>
-          );
-        } else {
-          warning(
-            true,
-            `<Router basepath="${basepath}">\n\nNothing matched:\n\t${
-              location.pathname
-            }\n\nPaths checked: \n\t${routes
-              .map(route => route.path)
-              .join(
-                "\n\t"
-              )}\n\nTo get rid of this warning, add a default NotFound component as child of Router:
+      return (
+        <BaseContext.Provider value={{ baseuri: uri, basepath }}>
+          {clone}
+        </BaseContext.Provider>
+      );
+    } else {
+      warning(
+        true,
+        `<Router basepath="${basepath}">\n\nNothing matched:\n\t${
+          location.pathname
+        }\n\nPaths checked: \n\t${routes
+          .map(route => route.path)
+          .join(
+            "\n\t"
+          )}\n\nTo get rid of this warning, add a default NotFound component as child of Router:
         \n\tlet NotFound = () => <div>Not Found!</div>
         \n\t<Router>\n\t  <NotFound default/>\n\t  {/* ... */}\n\t</Router>`
-          );
-          return null;
-        }
-      }
+      );
+      return null;
     }
-  )
-);
+  }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
-let Link = withBase(
-  withLocation(props => {
-    let {
-      basepath,
-      baseuri,
-      navigate,
-      location,
-      to,
-      state,
-      replace,
-      ...anchorProps
-    } = props;
+let Link = props => (
+  <BaseContext.Consumer>
+    {({ basepath, baseuri }) => (
+      <Location>
+        {({ navigate }) => {
+          let { location, to, state, replace, ...anchorProps } = props;
 
-    let href = resolve(to, baseuri);
+          let href = resolve(to, baseuri);
 
-    return (
-      <a
-        {...anchorProps}
-        href={href}
-        onClick={event => {
-          if (anchorProps.onClick) anchorProps.onClick(event);
-          if (shouldNavigate(event)) {
-            event.preventDefault();
-            navigate(href, { state, replace });
-          }
+          return (
+            <a
+              {...anchorProps}
+              href={href}
+              onClick={event => {
+                if (anchorProps.onClick) anchorProps.onClick(event);
+                if (shouldNavigate(event)) {
+                  event.preventDefault();
+                  navigate(href, { state, replace });
+                }
+              }}
+            />
+          );
         }}
-      />
-    );
-  })
+      </Location>
+    )}
+  </BaseContext.Consumer>
 );
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -244,24 +229,28 @@ let redirect = to => {
   throw new RedirectRequest(to);
 };
 
-let Redirect = withLocation(
-  class RedirectImpl extends React.Component {
-    // Support React < 16 with this hook
-    componentDidMount() {
-      let {
-        props: { navigate, to, from, replace = true, state, noThrow, ...props }
-      } = this;
-      navigate(insertParams(to, props), { replace, state });
-    }
-
-    render() {
-      let {
-        props: { navigate, to, from, replace, state, noThrow, ...props }
-      } = this;
-      if (!noThrow) redirect(insertParams(to, props));
-      return null;
-    }
+class RedirectImpl extends React.Component {
+  // Support React < 16 with this hook
+  componentDidMount() {
+    let {
+      props: { navigate, to, from, replace = true, state, noThrow, ...props }
+    } = this;
+    navigate(insertParams(to, props), { replace, state });
   }
+
+  render() {
+    let {
+      props: { navigate, to, from, replace, state, noThrow, ...props }
+    } = this;
+    if (!noThrow) redirect(insertParams(to, props));
+    return null;
+  }
+}
+
+let Redirect = props => (
+  <Location>
+    {locationContext => <RedirectImpl {...locationContext} {...props} />}
+  </Location>
 );
 
 Redirect.propTypes = {
@@ -283,20 +272,24 @@ Redirect.propTypes = {
 };
 
 ////////////////////////////////////////////////////////////////////////////////
-let Match = withLocation(({ path, location, navigate, children }) => {
-  let result = match(path, location.pathname);
-  return children({
-    navigate,
-    location,
-    match: result
-      ? {
-          ...result.params,
-          uri: result.uri,
-          path
-        }
-      : null
-  });
-});
+let Match = ({ path, children }) => (
+  <Location>
+    {({ navigate, location }) => {
+      let result = match(path, location.pathname);
+      return children({
+        navigate,
+        location,
+        match: result
+          ? {
+              ...result.params,
+              uri: result.uri,
+              path
+            }
+          : null
+      });
+    }}
+  </Location>
+);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Junk
