07608a62fad10236e2b300dd2dc5b993327026ba
kingdaro <kingdaro@gmail.com>
2018-06-03 19:19:38 -0400

    use optional props for path/default

--- types/index.d.ts
+++ types/index.d.ts
@@ -37,17 +37,13 @@ export interface LinkGetProps {
 
 export class Link<TState> extends React.Component<LinkProps<TState>> {}
 
-export type ExternalRouteProps = { path: string } | { default: boolean };
-
-export type InternalRouteProps = {
+export type RouteComponentProps<TParams = {}> = Partial<TParams> & {
+  path?: string;
+  default?: boolean;
   location?: WindowLocation;
   navigate?: NavigateFn;
   uri?: string;
 };
-
-export type RouteComponentProps<TParams = {}> = Partial<TParams> &
-  ExternalRouteProps &
-  InternalRouteProps;
 
 export interface RedirectProps {
   from?: string;
