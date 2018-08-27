14ada531b9244e3a5fa4ec756826b350494364c0
Rouven Weßling <rouven@contentful.com>
2018-08-27 20:33:46 +0200

    Use rollup for all builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,78 @@
 import babel from "rollup-plugin-babel";
-import uglify from "rollup-plugin-uglify";
+import { uglify } from "rollup-plugin-uglify";
 import replace from "rollup-plugin-replace";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
+import filesize from "rollup-plugin-filesize";
 
-const config = {
-  input: "src/index.js",
-  output: {
-    name: "ReachRouter",
-    globals: {
-      react: "React",
-      "react-dom": "ReactDOM"
-    }
-  },
-  external: ["react", "react-dom"],
-  plugins: [
-    babel({
-      exclude: "node_modules/**"
-    }),
-    resolve(),
-    commonjs({
-      include: /node_modules/
-    }),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-    })
-  ]
+import pkg from "./package.json";
+
+const dependencies = Object.keys(pkg.dependencies);
+const peerDependencies = Object.keys(pkg.peerDependencies);
+
+const input = "src/index.js";
+const umdConfig = {
+  name: "ReachRouter",
+  format: "umd",
+  globals: {
+    react: "React",
+    "react-dom": "ReactDOM"
+  }
 };
 
-if (process.env.NODE_ENV === "production") {
-  config.plugins.push(uglify());
-}
+const basePlugins = [
+  babel({
+    exclude: "node_modules/**"
+  }),
+  filesize()
+];
 
-export default config;
+const umdPlugins = [
+  ...basePlugins,
+  resolve(),
+  commonjs({
+    include: /node_modules/
+  }),
+  replace({
+    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+  })
+];
+
+export default [
+  {
+    input,
+    output: {
+      file: pkg.main,
+      format: "cjs"
+    },
+    external: [...dependencies, ...peerDependencies],
+    plugins: basePlugins
+  },
+  {
+    input,
+    output: {
+      file: pkg.module,
+      format: "es"
+    },
+    external: [...dependencies, ...peerDependencies],
+    plugins: basePlugins
+  },
+  {
+    input,
+    output: {
+      file: "umd/reach-router.js",
+      ...umdConfig
+    },
+    external: peerDependencies,
+    plugins: umdPlugins
+  },
+  {
+    input,
+    output: {
+      file: "umd/reach-router.min.js",
+      ...umdConfig
+    },
+    external: peerDependencies,
+    plugins: [...umdPlugins, uglify()]
+  }
+];
