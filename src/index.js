0c76d76a794af0dcec2ec1b908c8cad05ef8666d
Rouven Weßling <rouven@contentful.com>
2018-08-27 20:51:29 +0200

    Remove the depenency on warning as it's currently unused

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
-import warning from "warning";
 import PropTypes from "prop-types";
 import invariant from "invariant";
 import createContext from "create-react-context";
