246c7cbcca21279d68a14defe803436411944e9d
huangteng01 <huangteng01@baidu.com>
2019-08-21 16:48:45 +0800

    fixed for utils.js/resolve

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -162,7 +162,7 @@ let resolve = (to, base) => {
   }
 
   // profile, /users/789 => /users/789/profile
-  if (!startsWith(toSegments[0], ".")) {
+  if (!/\/?\./.test(toPathname)) {
     let pathname = baseSegments.concat(toSegments).join("/");
     return addQuery((basePathname === "/" ? "" : "/") + pathname, toQuery);
   }
@@ -172,7 +172,7 @@ let resolve = (to, base) => {
   // ../..      /users/123  =>  /
   // ../../one  /a/b/c/d    =>  /a/b/one
   // .././one   /a/b/c/d    =>  /a/b/c/one
-  let allSegments = baseSegments.concat(toSegments);
+  let allSegments = baseSegments.concat(toSegments).filter(Boolean);
   let segments = [];
   for (let i = 0, l = allSegments.length; i < l; i++) {
     let segment = allSegments[i];
@@ -242,9 +242,8 @@ let rankRoute = (route, index) => {
 let rankRoutes = routes =>
   routes
     .map(rankRoute)
-    .sort(
-      (a, b) =>
-        a.score < b.score ? 1 : a.score > b.score ? -1 : a.index - b.index
+    .sort((a, b) =>
+      a.score < b.score ? 1 : a.score > b.score ? -1 : a.index - b.index
     );
 
 let segmentize = uri =>
